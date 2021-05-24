8d13e9012fe039e5e466c15728a0b748842ee7f5
Yaksh Patel <yaksh3636@gmail.com>
2021-05-23 21:46:28 -0600

    Priority Queue with Max Heap Array approach

--- Priority Queue/MaxHeapPriorityQueue.cs
+++ Priority Queue/MaxHeapPriorityQueue.cs
@@ -1,70 +1,107 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Net;
+using System.Linq;
 
 namespace Priority_Queue
 {
     public class MaxHeapPriorityQueue<T> where T:IComparable<T>
     {
-        public Node<T> Root { get; set; }
-        
-        public int Count { get; set; }
 
-        private List<Node<T>> array = new List<Node<T>>();
+        private int count;
+        public int Count
+        {
+            get => count;
+            protected set => count = value;
+        }
+
+        private List<T> array = new List<T>();
 
         public void Add(T data)
         {
-            Root = InsertRecursive(Root, data);
+            array.Add(data);
+            count++;
+            HeapifyUp(Count - 1);
         }
 
-        private Node<T> InsertRecursive(Node<T> node, T data, Node<T> parent = null)
+        private void HeapifyUp(int currentIndex)
         {
-            if (node == null)
+            int parentIndex = (currentIndex - 1) / 2;
+
+            if (array[currentIndex].CompareTo(array[parentIndex]) > 0)
             {
-                Count++;
-                var n = new Node<T>(data, parent);
-                array.Add(n);
-                return Heapify(n, data);
+                Swap(ref array,currentIndex,parentIndex);
+                HeapifyUp(parentIndex);
+            }
+        }
+
+        private void Swap(ref List<T> list,int i1,int i2)
+        {
+            var temp = list[i1];
+            list[i1] = list[i2];
+            list[i2] = temp;
+        }
+
+        public T Peek()
+        {
+            return array[0];
+        }
+
+        public T Remove()
+        {
+            var tempRoot = array[0];
+            Swap(ref array,(Count-1),0);
+            array.RemoveAt(Count-1);
+            count--;
+            HeapifyDown(0, ref array, count);
+
+            return tempRoot;
+        }
+
+       /// <summary>
+       /// Heapify Down is used in the Remove method and The sort array method.
+       /// </summary>
+       /// <param name="i">The current index</param>
+       /// <param name="list"></param>
+       /// <param name="n">The size of the array</param>
+        private void HeapifyDown(int i, ref List<T> list, int n)
+        {
+            var largest = i;
+            var left = 2 * i + 1; 
+            var right = 2 * i + 2;
+
+            if (left < n && list[left].CompareTo(list[largest]) > 0)
+            {
+                largest = left;
             }
             
-            if (node.Left == null)
+            if (right < n && list[right].CompareTo(list[largest]) > 0)
             {
-                node.Left = InsertRecursive(node.Left,data, node);
-            }else if (node.Right == null)
-            {
-                node.Right = InsertRecursive(node.Right, data, node);
-            }else
-            {
-                node.Left = InsertRecursive(node.Left, data, node.Left);
+                largest = right;
             }
 
-            return node;
-        }
-
-        private Node<T> Heapify(Node<T> node, T data)
-        {
-            if (node.Parent != null)
+            if (largest != i)
             {
-                if (data.CompareTo(node.Parent.Data) > 0)
-                {
-                    node.Data = node.Parent.Data;
-                    node.Parent.Data = data;
-                    node.Parent = Heapify(node.Parent, data);
-                }
+                Swap(ref list, i,largest);
+                HeapifyDown(largest, ref list, n);
             }
-
-            return node;
         }
 
-        public Node<T> Peek()
+        public T[] ToSortedArray()
         {
-            return Root;
-        }
+            var tempArray = array;
+            var arrayCount = tempArray.Count;
 
-        public Node<T> Remove()
-        {
+            while (arrayCount > 0)
+            {
+                Swap(ref tempArray,(arrayCount-1),0);
+                arrayCount--;
+                HeapifyDown(0, ref tempArray,arrayCount);
+            }
+          
 
-            return null;
+            tempArray.Reverse();
+            return tempArray.ToArray();
         }
     }
 }
--- Priority Queue/Program.cs
+++ Priority Queue/Program.cs
@@ -7,10 +7,20 @@ namespace Priority_Queue
         public static void Main(string[] args)
         {
             MaxHeapPriorityQueue<int> priorityQueue = new MaxHeapPriorityQueue<int>();
-            priorityQueue.Add(10);
-            priorityQueue.Add(8);
-            priorityQueue.Add(6);
-            priorityQueue.Add(12);
+            priorityQueue.Add(2);
+            priorityQueue.Add(7);
+            priorityQueue.Add(26);
+            priorityQueue.Add(25);
+            priorityQueue.Add(19);
+            priorityQueue.Add(17);
+            priorityQueue.Add(1);
+            priorityQueue.Add(90);
+            priorityQueue.Add(3);
+            priorityQueue.Add(36);
+
+            //int i = priorityQueue.Remove();
+            
+            int[] i = priorityQueue.ToSortedArray();
             //priorityQueue.Add(2);
             
             
